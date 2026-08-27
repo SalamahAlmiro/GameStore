8b7e1a5766c6e20bc32ecebd32438ce426a5acc8
SalamahAlmiro <salamah-2002-@hotmail.com>
2026-08-28 00:58:16 +0300

    cleanup

--- WPF-LoginForm/Views/LibraryView.xaml.cs
+++ WPF-LoginForm/Views/LibraryView.xaml.cs
@@ -80,12 +80,12 @@ namespace WPF_LoginForm.Views
                     VerticalAlignment = VerticalAlignment.Center,
                     Margin = new Thickness(360, 0, 0, 0)
                 };
-                /*modernButton.Click += (s, e) =>
+                modernButton.Click += (s, e) =>
                 {
                     MessageBox.Show($"Launching {label.Text}");
-                };*/
+                };
 
-                modernButton.Click += (s, e) =>
+                /* modernButton.Click += (s, e) =>
                 {
                     try
                     {
@@ -98,7 +98,7 @@ namespace WPF_LoginForm.Views
                     {
                         MessageBox.Show($"Failed to launch the program: {ex.Message}");
                     }
-                };
+                }; */
 
                 Grid.SetColumn(modernButton, 1);
                 grid.Children.Add(modernButton);
